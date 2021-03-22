05f0e8d5e7726cb02f08365b5ee46e052a1f99ba
liufu.lf <liufu.lf@antfin.com>
2021-03-22 19:38:30 +0800

    init code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Line } from "@ant-design/charts";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Page = () => {
+  const data = [
+    { year: "1991", value: 3 },
+    { year: "1992", value: 4 },
+    { year: "1993", value: 3.5 },
+    { year: "1994", value: 5 },
+    { year: "1995", value: 4.9 },
+    { year: "1996", value: 6 },
+    { year: "1997", value: 7 },
+    { year: "1998", value: 9 },
+    { year: "1999", value: 13 },
+  ];
 
-export default App;
+  const config = {
+    data,
+    height: 400,
+    xField: "year",
+    yField: "value",
+    point: {
+      size: 5,
+      shape: "diamond",
+    },
+  };
+  return <Line {...config} />;
+};
+export default Page;
