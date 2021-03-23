223207a0bb8456ad004f192684902171b6751955
liufu.lf <liufu.lf@antfin.com>
2021-03-23 09:46:14 +0800

    test Pie

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
-import { Line } from "@ant-design/charts";
+import { Pie } from "@ant-design/charts";
 
 const Page = () => {
   const data = [
-    { year: "1991", value: 3 },
-    { year: "1992", value: 4 },
-    { year: "1993", value: 3.5 },
-    { year: "1994", value: 5 },
-    { year: "1995", value: 4.9 },
-    { year: "1996", value: 6 },
-    { year: "1997", value: 7 },
-    { year: "1998", value: 9 },
-    { year: "1999", value: 13 },
+    {
+      type: "分类一",
+      value: 27,
+    },
+    {
+      type: "分类二",
+      value: 25,
+    },
+    {
+      type: "分类三",
+      value: 18,
+    },
+    {
+      type: "分类四",
+      value: 15,
+    },
+    {
+      type: "分类五",
+      value: 10,
+    },
+    {
+      type: "其他",
+      value: 5,
+    },
   ];
 
   const config = {
     data,
-    height: 400,
-    xField: "year",
-    yField: "value",
-    point: {
-      size: 5,
-      shape: "diamond",
+    angleField: "value",
+    colorField: "type",
+    radius: 0.9,
+    label: {
+      type: "inner",
+      offset: "-30%",
+      content: function content(_ref) {
+        var percent = _ref.percent;
+        return "".concat(percent * 100, "%");
+      },
+      style: {
+        fontSize: 14,
+        textAlign: "center",
+      },
     },
+    interactions: [{ type: "element-active" }],
   };
-  return <Line {...config} />;
+  return <Pie {...config} />;
 };
 export default Page;
